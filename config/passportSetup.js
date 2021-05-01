4870dc5b7e7068e0feb9913002704261595ae1de
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-01 21:40:56 +0100

    added adding and retrrieving user to/from db

--- config/passportSetup.js
+++ config/passportSetup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/userModel');
 
 passport.use(new GoogleStrategy({
     //options for google strategy
@@ -8,6 +9,22 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.google.clientSecret,
     callbackURL: '/auth/googleAuth/redirect'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
- })
+
+    User.findOne({
+        googleID: profile.id
+    }).then((userFound) => {
+        if (userFound) {
+            console.log(`User already exists - ${userFound}`);
+        } else {
+            new User({
+                username: profile.displayName,
+                googleID: profile.id
+            }).save()
+                .then((newUser) => {
+                    console.log(`New user ${newUser}`);
+                });
+
+        }
+    });
+})
 );
